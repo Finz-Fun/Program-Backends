f7bd0d4fb5eb44b6d1f450145cd630bcb6c44af2
Ramik Mukherjee <ramik.14062000@gmail.com>
2025-01-22 21:55:43 +0530

    cors changed

--- app/solana-token-backend/src/index.ts
+++ app/solana-token-backend/src/index.ts
@@ -11,7 +11,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.use('/blinks', actionCorsMiddleware({headers: ACTIONS_CORS_HEADERS,chainId: BLOCKCHAIN_IDS.devnet,actionVersion:1}));
